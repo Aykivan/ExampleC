72e6428d8be560410e1446bfc0c1e9db0343781d
Айкин Иван <ivan.aykin@mail.ru>
2022-11-17 17:59:14 +0700

    added the result of 2 lectures

---
+++ Example10_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 22, 34, 45, 52, 26, 75, 28 };
+
+int n = array.Length;
+int find = 75;
+
+int index = 0;
+
+while (index < n)
+{
+    if (find == array[index])
+    {
+        System.Console.WriteLine(index);
+        break; //прерви
+    }
+    index++;
+}
---
+++ Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection) //FillArray - заполнить массив, в данном случае без вывода его результатов на экран
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //это метод который будет выводить числа массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //специально задаем данное значение, чтобы, если отсуствует искомый элемент, мы видели это в виде -1
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //Создает новый массив с 10тью числами, и заполняет его нулями
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos= IndexOff(array, 4);
+System.Console.WriteLine(pos);
