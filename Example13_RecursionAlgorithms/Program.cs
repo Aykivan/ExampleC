6aa7c47445dae689ab4901fb87aeb13f389e5923
Айкин Иван <ivan.aykin@mail.ru>
2022-12-05 10:52:03 +0700

    added lecture 4

---
+++ Example13_RecursionAlgorithms/Program.cs
@@ -0,0 +1,119 @@
+﻿// Двумерные масивы
+
+// table[0,0] table[0,1] table[0,2] table[0,3] table[0,4]
+// table[1,0] table[1,1] table[1,2] table[1,3] table[1,4]
+
+// string[,] table = new string[2, 5];
+// table[1, 2] = "слово"; // 1 цифра в скобках - строка, 2 - столбец
+
+/* for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+} */
+
+int[,] RandomRecursion(int rows, int columns)
+{
+    int[,] recursion = new int[rows, columns];
+    for (int i = 0; i < recursion.GetLength(0); i++)
+    {
+        for (int j = 0; j < recursion.GetLength(1); j++)
+        {
+            recursion[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return recursion;
+}
+
+void Print(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++) // GetLenhts(0) - означает обращение к первому значению в скобках [3]
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++) // GetLenghts(1) - обращение ко 2 элементу в скобках [4]
+            System.Console.Write($"{matrix[rows, columns]} ");
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = RandomRecursion(3, 4);
+// Print(matrix);
+
+// Закрашиваем руку
+
+int[,] image = new int[,]
+{
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void ImagePrint(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i, j] == 0) System.Console.Write($" ");
+            else System.Console.Write($"+");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// Рекурсия - функция, которая вызывает сама себя!
+void FillImage(int row, int col)
+{
+    if (image[row, col] == 0)
+    {
+        image[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+// ImagePrint(image);
+// FillImage(13,13);
+// ImagePrint(image);
+
+/* double Factorial(int n)
+{
+    // 1!=1 0!=1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+} */
+
+// for (int i = 0; i < 10; i++) System.Console.WriteLine(Factorial(i));
+
+// Фибоначчи
+int Fib(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fib(n - 1) + Fib(n - 2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    System.Console.Write($" {Fib(i)}");
+}
